e4d90037ddc432f2e4c11cb054ed25a6427d0181
Andy Blackburn <andyblac@o2.co.uk>
2012-03-27 01:58:49 +0100

    remove some debug code.

--- src/__init__.py
+++ src/__init__.py
@@ -9,14 +9,14 @@ PluginLanguagePath = "Extensions/EPGImport/po"
 def localeInit():
 	lang = language.getLanguage()[:2] # getLanguage returns e.g. "fi_FI" for "language_country"
 	os.environ["LANGUAGE"] = lang # Enigma doesn't set this (or LC_ALL, LC_MESSAGES, LANG). gettext needs it!
-	print "[XMLTV-Import] set language to ", lang
+# 	print "[XMLTV-Import] set language to ", lang
 	gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
 	gettext.bindtextdomain('enigma2', resolveFilename(SCOPE_LANGUAGE, ""))
 
 def _(txt):
 	t = gettext.dgettext(PluginLanguageDomain, txt)
 	if t == txt:
-		print "[EPGImport] fallback to default translation for", txt
+# 		print "[EPGImport] fallback to default translation for", txt
 		t = gettext.dgettext('enigma2', txt)
 	return t
 
