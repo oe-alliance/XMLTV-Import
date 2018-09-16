d442513f7533a6b18d462a81b2dbf23dab29ff8d
Schimmelreiter <Ziggy.SpaceRat@gmx.de>
2018-09-16 12:03:37 +0200

    Fix merge
    Re-add
    https://github.com/oe-alliance/XMLTV-Import/commit/88ca59a3aa3857a3c950fcf2d193d319869a9db8#diff-1eb898651d0ba947813716e7dbad6c99
    lost in
    https://github.com/oe-alliance/XMLTV-Import/commit/2e43d643e5d570ea8a3e581017c66ffad01585c1

--- src/EPGImport/EPGImport.py
+++ src/EPGImport/EPGImport.py
@@ -410,6 +410,10 @@ class EPGImport:
 	def isImportRunning(self):
 		return self.source is not None
 
+	def legacyDownload(self, result, afterDownload, downloadFail, sourcefile, filename, deleteFile=True):
+		print>>log, "[EPGImport] IPv6 download failed, falling back to IPv4: " + sourcefile
+		downloadPage(sourcefile, filename).addCallbacks(afterDownload, downloadFail, callbackArgs=(filename,True))
+
 	def do_download(self, sourcefile, afterDownload, downloadFail):
 		path = bigStorage(9000000, '/tmp', '/media/DOMExtender', '/media/cf', '/media/mmc', '/media/usb', '/media/hdd')
 		filename = os.path.join(path, 'epgimport')
@@ -419,13 +423,17 @@ class EPGImport:
 			filename += ext
 		sourcefile = sourcefile.encode('utf-8')
 		print>>log, "[EPGImport] Downloading: " + sourcefile + " to local path: " + filename
-		if self.source.nocheck == 1:
-			print>>log, "[EPGImport] Not cheching the server since nocheck is set for it: " + sourcefile
-			downloadPage(sourcefile, filename).addCallbacks(afterDownload, downloadFail, callbackArgs=(filename,True))
+		if self.checkValidServer(sourcefile) == 1:
+			if has_ipv6 and version_info >= (2,7,11) and ((version.major == 15 and version.minor >= 5) or version.major >= 16):
+				host = sourcefile.split("/")[2]
+				ip6 = getaddrinfo(host,0, AF_INET6)
+				if ip6:
+					sourcefile6 = sourcefile.replace(host,"[" + list(ip6)[0][4][0] + "]")
+					print>>log, "[EPGImport] Trying IPv6 first: " + sourcefile6
+					downloadPage(sourcefile6, filename, headers={'host': host}).addCallback(afterDownload, filename, True).addErrback(self.legacyDownload, afterDownload, downloadFail, sourcefile, filename, True)
+			else:
+				print>>log, "[EPGImport] No IPv6, using IPv4 directly: " + sourcefile
+				downloadPage(sourcefile, filename).addCallbacks(afterDownload, downloadFail, callbackArgs=(filename,True))
 			return filename
 		else:
-			if self.checkValidServer(sourcefile) == 1:
-				downloadPage(sourcefile, filename).addCallbacks(afterDownload, downloadFail, callbackArgs=(filename,True))
-				return filename
-			else:
-				self.downloadFail("checkValidServer reject the server")
+			self.downloadFail("checkValidServer reject the server")
