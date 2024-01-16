b6c899fbd53044410c33b9b99e9f0fb810e9673e
Captain <captain.onboard@web.de>
2024-01-16 18:35:41 +0100

    distutils are outdated use setuptools

--- src/setup_translate.py
+++ src/setup_translate.py
@@ -1,13 +1,11 @@
 from __future__ import print_function
-# Language extension for distutils Python scripts. Based on this concept:
-# http://wiki.maemo.org/Internationalize_a_Python_application
-from distutils import cmd
-from distutils.command.build import build as _build
+from setuptools import Command
+from setuptools.command.build import build as _build
 import glob
 import os
 
 
-class build_trans(cmd.Command):
+class build_trans(Command):
 	description = 'Compile .po files into .mo files'
 
 	def initialize_options(self):
